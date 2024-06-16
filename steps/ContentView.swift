82b7f1abdab0049bc601dd842f9e885a6739e571
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-16 13:15:59 +0200

    Embed card in scrollview

--- steps/ContentView.swift
+++ steps/ContentView.swift
@@ -10,29 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            VStack(spacing: 20) {
-                VStack {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Label("Steps", systemImage: "figure.walk")
-                                .font(.title3.bold())
-                                .foregroundStyle(.cyan)
-                            Text("Avg: 10K Steps")
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
+            ScrollView {
+                VStack(spacing: 20) {
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.cyan)
+                                Text("Avg: 10K Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            Spacer()
                         }
-                        Spacer()
                     }
+                    
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(.secondary)
+                        .frame(height: 150)
                 }
-                
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundStyle(.secondary)
-                    .frame(height: 150)
-            }
-            .padding()
-            .background {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(Color(.secondarySystemBackground))
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color(.secondarySystemBackground))
+                }
             }
             .padding()
             .navigationTitle("Dashboard")
