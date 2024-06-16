d8ddbbebacc9f29e958f7c143a53de36fbf34eb5
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-16 12:49:03 +0200

    setup project

---
+++ steps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  steps
+//
+//  Created by Julian Schenkemeyer on 16.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ steps/stepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepsApp.swift
+//  steps
+//
+//  Created by Julian Schenkemeyer on 16.06.24.
+//
+
+import SwiftUI
+
+@main
+struct stepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
