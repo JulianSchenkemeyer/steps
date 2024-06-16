b1dc8cd51983516b1bcb9dd60002d9f37bfc9725
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-16 13:22:54 +0200

    Add NavigationDestionation

--- steps/ContentView.swift
+++ steps/ContentView.swift
@@ -13,17 +13,21 @@ struct ContentView: View {
             ScrollView {
                 VStack(spacing: 20) {
                     VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.cyan)
-                                Text("Avg: 10K Steps")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
+                        NavigationLink(value: "Steps") {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Steps", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.cyan)
+                                    Text("Avg: 10K Steps")
+                                        .font(.caption)
+                                }
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
                             }
-                            Spacer()
                         }
+                        .foregroundStyle(.secondary)
                     }
                     
                     RoundedRectangle(cornerRadius: 12)
@@ -38,6 +42,9 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: String.self) { metric in
+                Text(metric)
+            }
         }
     }
 }
