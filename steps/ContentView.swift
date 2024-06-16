0dddd22b7d7c3c058f8cb41ef4593a1a89bb78cd
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-16 13:14:19 +0200

    create initial card prototype

--- steps/ContentView.swift
+++ steps/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing: 20) {
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Label("Steps", systemImage: "figure.walk")
+                                .font(.title3.bold())
+                                .foregroundStyle(.cyan)
+                            Text("Avg: 10K Steps")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        Spacer()
+                    }
+                }
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.secondary)
+                    .frame(height: 150)
+            }
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color(.secondarySystemBackground))
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
