040b59b63b009d4fe0b3059c410601c624a75a4b
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-17 12:04:37 +0200

    Create HealtKitManager class

---
+++ steps/HealthKitManager.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitManager.swift
+//  steps
+//
+//  Created by Julian Schenkemeyer on 17.06.24.
+//
+
+import Foundation
+import HealthKit
+import Observation
+
+
+@Observable
+class HealthKitManager {
+    
+    let store = HKHealthStore()
+}
