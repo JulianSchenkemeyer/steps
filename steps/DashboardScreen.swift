252e1afc0cba10e9e94346d64991e5a7b99f7207
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-22 18:10:50 +0200

    add annotation to chart

--- steps/DashboardScreen.swift
+++ steps/DashboardScreen.swift
@@ -14,11 +14,17 @@ struct DashboardScreen: View {
     
     @State private var isShowingPermissionPrimingSheet = false
     @State private var steps: [HealthMetric] = []
+    @State private var selectedDate: Date?
     
     var averageSteps: Double {
         guard !steps.isEmpty else { return 0 }
         let totalSteps = steps.reduce(0.0) { $0 + $1.value }
         return totalSteps / Double(steps.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let selectedDate else { return nil }
+        return steps.first { Calendar.current.isDate($0.date, inSameDayAs: selectedDate) }
     }
     
     var body: some View {
@@ -43,19 +49,37 @@ struct DashboardScreen: View {
                         .foregroundStyle(.secondary)
                     }
                     
-                    Chart(steps) { step in
+                    Chart {
+                        if let selectedHealthMetric {
+                            RuleMark(x: .value("Selected Metric",
+                                               selectedHealthMetric.date,
+                                               unit: .day))
+                            .offset(y: -10)
+                            .annotation(position: .top,
+                                        alignment: .center,
+                                        spacing: 0,
+                                        overflowResolution: .init(x: .fit(to: .chart))) {
+                                annotationView
+                            }
+                        }
+                        
                         RuleMark(y: .value("Average", averageSteps))
-                            .foregroundStyle(Color.secondary)
+                            .foregroundStyle(.black.opacity(0.75))
                             .lineStyle(.init(lineWidth: 1, dash: [7]))
                         
-                        BarMark(
-                            x: .value("Date",
-                                      step.date,
-                                      unit: .day),
-                            y: .value("Count", step.value)
-                        )
-                        .foregroundStyle(Color.cyan.gradient)
+                        ForEach(steps) { step in
+                            BarMark(
+                                x: .value("Date",
+                                          step.date,
+                                          unit: .day),
+                                y: .value("Count", step.value)
+                            )
+                            .foregroundStyle(Color.cyan.gradient)
+                            .opacity(selectedDate == nil || selectedHealthMetric?.date == step.date ? 1 : 0.2)
+                        }
                     }
+                    .foregroundStyle(.secondary.opacity(0.2))
+                    .chartXSelection(value: $selectedDate.animation(.easeInOut))
                     .chartXAxis {
                         AxisMarks {
                             AxisValueLabel(format: .dateTime.day().month(.defaultDigits))
@@ -64,7 +88,6 @@ struct DashboardScreen: View {
                     .chartYAxis {
                         AxisMarks { value in
                             AxisGridLine()
-                                .foregroundStyle(Color.secondary.opacity(0.2))
                             AxisValueLabel((value.as(Double.self) ?? 0).formatted(.number.notation(.compactName)))
                         }
                     }
@@ -73,7 +96,7 @@ struct DashboardScreen: View {
                 .padding()
                 .background {
                     RoundedRectangle(cornerRadius: 12)
-                        .fill(Color(.secondarySystemBackground))
+                        .fill(Material.thin)
                 }
             }
             .padding()
@@ -92,6 +115,24 @@ struct DashboardScreen: View {
             }
         }
         .tint(.cyan)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.day().month(.abbreviated).weekday(.abbreviated))
+                .font(.footnote.bold())
+                .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.cyan)
+        }
+        .padding(8)
+        .background {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color(uiColor: .secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.2),radius: 1, x: 1, y: 1)
+        }
     }
 }
 
