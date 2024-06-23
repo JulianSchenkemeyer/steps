cd7e98357c8ca87ccff7b3f550ec493744891d50
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-06-23 11:49:56 +0200

    add ability to select slices of the averageStepsPerWeekdayView chart

--- steps/Views/AverageStepsPerWeekdayView.swift
+++ steps/Views/AverageStepsPerWeekdayView.swift
@@ -12,6 +12,22 @@ import Charts
 struct AverageStepsPerWeekdayView: View {
     var chartData: [HealthMetric]
     
+    @State private var selectedData: Double?
+    
+    var selectedWeekday: HealthMetric? {
+        guard let selectedData else { return nil }
+        var total = 0.0
+        
+        return chartData.first {
+            total += $0.value
+            return selectedData <= total
+        }
+    }
+    
+    func isSelected(_ weekday: HealthMetric) -> Bool {
+        selectedWeekday == nil || selectedWeekday?.date == weekday.date
+    }
+    
     var body: some View {
         VStack {
             VStack(spacing: 20) {
@@ -36,10 +52,14 @@ struct AverageStepsPerWeekdayView: View {
                     ForEach(chartData) { weekday in
                         SectorMark(angle: .value("Average Steps", weekday.value),
                                    innerRadius: .ratio(0.65),
+                                   outerRadius:  isSelected(weekday) ? 130 : 110,
                                    angularInset: 1)
                         .cornerRadius(5)
+                        .foregroundStyle(Color.cyan.gradient)
+                        .opacity(isSelected(weekday) ? 1 : 0.2)
                     }
                 }
+                .chartAngleSelection(value: $selectedData.animation(.easeInOut))
             }
         }
         .frame(height: 300)
