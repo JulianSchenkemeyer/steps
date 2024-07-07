5364a6429c2590cb78a03d4c7d67f7c98bc9e5d7
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-07-07 17:12:49 +0200

    throw HealthManagerErrors in HealthKitManager

--- steps/Managers/HealthKitManager.swift
+++ steps/Managers/HealthKitManager.swift
@@ -15,7 +15,15 @@ class HealthKitManager {
     let store = HKHealthStore()
     let types: Set = [HKQuantityType(.stepCount)]
     
-    func fetchStepsData() async -> [HealthMetric] {
+    func getAuthorizationStatus() -> HKAuthorizationStatus {
+        store.authorizationStatus(for: HKQuantityType(.stepCount))
+    }
+    
+    func fetchStepsData() async throws -> [HealthMetric] {
+        guard getAuthorizationStatus() != .notDetermined else {
+            throw HealthManagerError.authorizationNotDetermined
+        }
+        
         let calendar = Calendar.current
         let today = calendar.startOfDay(for: .now)
         let end = calendar.date(byAdding: .day, value: 1, to: today)!
@@ -25,34 +33,50 @@ class HealthKitManager {
         let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.stepCount),
                                                                predicate: queryPredicate)
         let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate,
-                                                                     options: .cumulativeSum,
-                                                                     anchorDate: end,
-                                                                     intervalComponents: .init(day: 1))
+                                                               options: .cumulativeSum,
+                                                               anchorDate: end,
+                                                               intervalComponents: .init(day: 1))
         
-        guard let result = try? await stepsQuery.result(for: store) else {
-            return []
+        do {
+            let result = try await stepsQuery.result(for: store)
+            let healthMetrics = result.statistics().map {
+                HealthMetric(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+            
+            return healthMetrics
+        } catch {
+            throw HealthManagerError.unableToCompleteRequest
         }
-        print("✅ fetched steps")
-        
-        let healthMetrics = result.statistics().map {
-            HealthMetric(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
-        }
-        
-        return healthMetrics
     }
     
-    func addStepData(date: Date, value: Double) async {
+    func addStepData(date: Date, value: Double) async throws {
+        let status = getAuthorizationStatus()
+        switch status {
+        case .notDetermined:
+            throw HealthManagerError.authorizationNotDetermined
+        case .sharingDenied:
+            throw HealthManagerError.sharingDenied
+        case .sharingAuthorized:
+            break
+        @unknown default:
+            break
+        }
+        
         let stepValue = HKQuantity(unit: .count(), doubleValue: value)
         let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount), quantity: stepValue, start: date, end: date)
         
-        try! await store.save(stepSample)
+        do {
+            try await store.save(stepSample)
+        } catch {
+            throw HealthManagerError.unableToCompleteRequest
+        }
     }
     
-    #if targetEnvironment(simulator)
+#if targetEnvironment(simulator)
     func addMockData() async {
         var mockSample: [HKQuantitySample] = []
         
-        for i in 0..<28 {
+        for i in 0..<5 {
             let stepValue = HKQuantity(unit: .count(), doubleValue: .random(in: 2_000...20_000))
             let date = Calendar.current.date(byAdding: .day, value: -i, to: .now)!
             let stepSample = HKQuantitySample(type: HKQuantityType(.stepCount),
@@ -65,5 +89,5 @@ class HealthKitManager {
         
         try! await store.save(mockSample)
     }
-    #endif
+#endif
 }
