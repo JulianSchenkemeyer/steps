fa75b6af10ed7f6a039e3997a05f90ac12bbb2cf
Julian Schenkemeyer <julian@jschenkemeyer.com>
2024-07-15 22:25:30 +0200

    refactor AverageStepsPerWeekdayView and StepChartView to use ChartContainer

--- steps/Views/AverageStepsPerWeekdayView.swift
+++ steps/Views/AverageStepsPerWeekdayView.swift
@@ -29,79 +29,56 @@ struct AverageStepsPerWeekdayView: View {
     }
     
     var body: some View {
-        VStack {
-            VStack(spacing: 20) {
-                VStack {
-                    NavigationLink(value: "Steps") {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Averages", systemImage: "calendar")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.cyan)
-                                
-                                Text("Last 28 Days")
-                                    .font(.caption)
-                            }
-                            Spacer()
-                        }
+        ChartContainer(title: "Averages", systemImage: "calendar", subtitle: "Last 28 Days") {
+            
+            if chartData.isEmpty {
+                ChartDataUnavaibleView(systemImage: "chart.pie",
+                                       title: "No Data",
+                                       description: "There is no step count data from the Health App")
+            } else {
+                Chart {
+                    ForEach(chartData) { weekday in
+                        SectorMark(angle: .value("Average Steps", weekday.value),
+                                   innerRadius: .ratio(0.65),
+                                   outerRadius:  isSelected(weekday) ? 130 : 110,
+                                   angularInset: 1)
+                        .cornerRadius(5)
+                        .foregroundStyle(Color.cyan.gradient)
+                        .opacity(isSelected(weekday) ? 1 : 0.2)
                     }
-                    .foregroundStyle(.secondary)
                 }
-                
-                if chartData.isEmpty {
-                    ChartDataUnavaibleView(systemImage: "chart.pie",
-                                           title: "No Data",
-                                           description: "There is no step count data from the Health App")
-                } else {
-                    Chart {
-                        ForEach(chartData) { weekday in
-                            SectorMark(angle: .value("Average Steps", weekday.value),
-                                       innerRadius: .ratio(0.65),
-                                       outerRadius:  isSelected(weekday) ? 130 : 110,
-                                       angularInset: 1)
-                            .cornerRadius(5)
-                            .foregroundStyle(Color.cyan.gradient)
-                            .opacity(isSelected(weekday) ? 1 : 0.2)
-                        }
+                .chartAngleSelection(value: $selectedData.animation(.easeInOut))
+                .onChange(of: selectedData) { old, new in
+                    if new == nil {
+                        selectedData = old
                     }
-                    .chartAngleSelection(value: $selectedData.animation(.easeInOut))
-                    .onChange(of: selectedData) { old, new in
-                        if new == nil {
-                            selectedData = old
-                        }
-                    }
-                    .chartBackground { proxy in
-                        GeometryReader { geometry in
-                            if let plotFrame = proxy.plotFrame, let selectedWeekday {
-                                let frame = geometry[plotFrame]
-                                VStack {
-                                    Text(selectedWeekday.date.weekdayTitle)
-                                        .font(.title3.bold())
-                                        .contentTransition(.identity)
-                                    
-                                    Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
-                                        .contentTransition(.numericText())
-                                        .fontWeight(.medium)
-                                        .foregroundStyle(.secondary)
-                                }
-                                .position(x: frame.midX, y: frame.midY)
+                }
+                .chartBackground { proxy in
+                    GeometryReader { geometry in
+                        if let plotFrame = proxy.plotFrame, let selectedWeekday {
+                            let frame = geometry[plotFrame]
+                            VStack {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .contentTransition(.identity)
+                                
+                                Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+                                    .contentTransition(.numericText())
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
                             }
+                            .position(x: frame.midX, y: frame.midY)
                         }
                     }
-                    .sensoryFeedback(.selection, trigger: selectedWeekday) { oldValue, newValue in
-                        guard let oldDate = oldValue?.date, let newDate = newValue?.date else { return false
-                        }
-                        return oldDate != newDate
+                }
+                .sensoryFeedback(.selection, trigger: selectedWeekday) { oldValue, newValue in
+                    guard let oldDate = oldValue?.date, let newDate = newValue?.date else { return false
                     }
+                    return oldDate != newDate
                 }
             }
         }
         .frame(height: 320)
-        .padding()
-        .background {
-            RoundedRectangle(cornerRadius: 12)
-                .fill(Material.thin)
-        }
     }
 }
 
--- steps/Views/ChartContainer.swift
+++ steps/Views/ChartContainer.swift
@@ -15,7 +15,7 @@ struct ChartContainer<Content: View>: View {
     @ViewBuilder var content: () -> Content
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        ChartContainerBase {
             HStack {
                 VStack(alignment: .leading) {
                     Label(title, systemImage: systemImage)
@@ -28,8 +28,53 @@ struct ChartContainer<Content: View>: View {
                 Spacer()
             }
             .foregroundStyle(.secondary)
-            
-            
+        } content: {
+            content()
+        }
+        
+    }
+}
+
+struct ChartContainerWithNavigation<Content: View>: View {
+    let title: String
+    let systemImage: String
+    let subtitle: String
+    let context: String
+    
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        ChartContainerBase {
+            NavigationLink(value: context) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Label(title, systemImage: systemImage)
+                            .font(.title3.bold())
+                            .foregroundStyle(.cyan)
+                        
+                        Text(subtitle)
+                            .font(.caption)
+                    }
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                }
+            }
+            .foregroundStyle(.secondary)
+        } content: {
+            content()
+        }
+        
+    }
+}
+
+struct ChartContainerBase<Header: View, Content: View>: View {
+    @ViewBuilder var header: () -> Header
+    @ViewBuilder var content: () -> Content
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            header()
             content()
         }
         .padding()
@@ -41,8 +86,20 @@ struct ChartContainer<Content: View>: View {
 }
 
 #Preview {
-    ChartContainer(title: "Chart Container", systemImage: "calendar", subtitle: "subtitle") {
-        Text("Chart")
-            .frame(height: 180)
+    VStack {
+        ChartContainer(title: "Chart Container",
+                       systemImage: "calendar",
+                       subtitle: "subtitle") {
+            Text("Chart")
+                .frame(height: 180)
+        }
+        
+        ChartContainerWithNavigation(title: "Chart Container with Navigation",
+                                     systemImage: "calendar",
+                                     subtitle: "subtitle",
+                                     context: "test") {
+            Text("Chart")
+                .frame(height: 180)
+        }
     }
 }
